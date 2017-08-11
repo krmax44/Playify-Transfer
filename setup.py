248a7e77ebfdb4c8793c0111b82e4c0aaa501d6c
Max <hi@krmax44.de>
2017-08-11 17:09:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from cx_Freeze import setup, Executable
 import os
 
-os.environ['TCL_LIBRARY']="C:\\Users\\Max\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
-os.environ['TK_LIBRARY']="C:\\Users\\Max\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
-
 executables = [Executable("playify-transfer.py", base="Win32GUI", targetName="playify-transfer-invisible.exe"), Executable("playify-transfer.py", base=None), Executable("password.py", base=None)]
 
 options = {
